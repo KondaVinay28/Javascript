48f7d0953de0dac2e763656a75a05073010524f0
KondaVinay28 <kondavinay555@gmail.com>
2024-12-13 20:59:47 +0530

    added DOM

---
+++ Playground.js
@@ -0,0 +1,24 @@
+// foreach 
+// every
+// filter 
+// reduce 
+// Map
+// sum 
+const array = [1, 2, 3, 4, 5];
+array.map((el) => {
+    return el*2;
+});
+//Array of objects
+const student = [
+    {
+        name : "konda",
+        marks : 89,
+    },
+    {
+        name : "Vinay",
+        marks : 99,
+    },
+];
+student.forEach((el) => {
+    console.log(el.name);
+});
