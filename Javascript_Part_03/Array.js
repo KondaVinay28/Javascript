00141d7e1955a0d7ea8d17b4e238645eb959a610
KondaVinay28 <kondavinay555@gmail.com>
2026-01-06 21:06:16 +0530

    Added Spread Operator in Arrays

--- Javascript_Part_03/Array.js
+++ Javascript_Part_03/Array.js
@@ -1,2 +1,6 @@
 let students = ["Tony Stark", "Flash", "SpiderMan", "Hulk"];
 console.log(students[0].length);
+/* Spread Operator */
+let updatedStudents = [...students, "SuperMan"];
+console.log(updatedStudents);
+console.log(students);
